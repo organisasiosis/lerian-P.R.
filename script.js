d7d398a67bad70bd3cac86c1dd039ab379917b96
organisasiosis <leriandika109@gmail.com>
2025-12-03 11:35:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,23 @@ function hitungKeuangan() {
     let target = convertNumber(document.getElementById("target").value);
 
     if (pendapatan <= 0 || pengeluaran <= 0 || target <= 0) {
-        alert("Isi semua data dengan benar!");
+        alert("Harap isi semua data dengan benar!");
         return;
     }
 
     let sisa = pendapatan - pengeluaran;
     if (sisa <= 0) {
-        alert("Pengeluaran Anda lebih besar dari pendapatan!");
+        alert("Pengeluaran lebih besar dari pendapatan!");
         return;
     }
 
     let bulan = (target / sisa).toFixed(1);
+    let tahun = Math.floor(bulan / 12);
+    let sisaBulan = Math.round(bulan % 12);
     let persen = Math.min((sisa / target) * 100, 100).toFixed(1);
 
     document.getElementById("sisa").innerHTML = `Rp ${new Intl.NumberFormat("id-ID").format(sisa)}`;
-    document.getElementById("waktu").innerHTML = `${bulan} bulan`;
+    document.getElementById("waktu").innerHTML = `${bulan} bulan (${tahun} tahun ${sisaBulan} bulan)`;
     document.getElementById("persen").innerHTML = `${persen}%`;
 
     document.getElementById("progress").style.width = `${persen}%`;
